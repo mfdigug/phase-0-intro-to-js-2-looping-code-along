075fd35d79f0c40a0a6a25d3f6b16c5c38ce58b7
Michelle Di Guglielmo <mfdigug@gmail.com>
2024-11-05 00:32:19 +1100

    done with looping code along

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+
+function writeCards(name, event) {
+    let message = []; 
+    for (let i = 0; i < name.length; i++) { 
+    message.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+      return message;
+    }
+        
+    writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function countDown() {
+    let countDown = 10;
+while (countDown >= 0) {
+  console.log(countDown--);
+}
+}
